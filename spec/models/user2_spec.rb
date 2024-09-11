c8cc4281246661c03a28d98f41a5cabd184fa444
Takuya Shiotari <ptn-takuya.shiotari@zozo.com>
2024-09-11 14:01:59 +0900

    Add tests

---
+++ spec/models/user2_spec.rb
@@ -0,0 +1,17 @@
+RSpec.describe 'User2' do
+  describe '#valid?' do
+    context 'when name is blank' do
+      it do
+        user = User.new(name: '')
+        expect(user.valid?).to be false
+      end
+    end
+
+    context 'when name is presence' do
+      it do
+        user = User.new(name: 'name')
+        expect(user.valid?).to be true
+      end
+    end
+  end
+end
