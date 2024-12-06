ce6c549923a66f543ff32f3cd8235e8b8d82f388
Takuya Shiotari <ptn-takuya.shiotari@zozo.com>
2024-12-06 14:07:37 +0900

    Coverage diff test

--- app/models/article.rb
+++ app/models/article.rb
@@ -3,4 +3,16 @@ class Article < ApplicationRecord
 
   validates :title, presence: true, length: { maximum: 64, allow_blank: true }
   validates :content, presence: true
+
+  def bar
+    "#{title} bar"
+  end
+
+  def baz
+    "#{title} baz"
+  end
+
+  def hoge
+    'hoge'
+  end
 end
